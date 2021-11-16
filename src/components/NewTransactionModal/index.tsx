324fb5c9fcc576aa2058e863cabc7ac4efa875f4
gilmarferrini <ferrinigilmar@gmail.com>
2021-11-16 18:14:01 -0300

    criando hook useTransactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -6,7 +6,7 @@ import outcomeImg from '../../assets/income.svg'
 import closeImg from '../../assets/close.svg'
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
-import { TransactionsContext } from '../../TransactionsContext'
+import { useTransactions } from '../../hooks/useTransactions'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions()
   
   const [title, setTitle] = useState('')
   const [amount, setAmount] = useState(0)
